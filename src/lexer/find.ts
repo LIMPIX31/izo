7fd9c84e9933115aa06a730fc2f4f128ed739204
LIMPIX31 <limpix31@gmail.com>
2022-10-30 11:18:35 +0200

    feat: add more find utils

--- src/lexer/find.ts
+++ src/lexer/find.ts
@@ -1,2 +1,7 @@
 export const take = (regex: RegExp, group = 0) => (src: string) =>
   src.match(regex)?.[group]?.length ?? -1
+
+export const single = (symbol: string) => (src: string) =>
+  src[0] === symbol ? 1 : -1
+
+export const eof = (src: string) => src.length === 0 ? 0 : -1
