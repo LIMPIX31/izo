2bc901d8220e69cb28ca997aded3412741749b89
LIMPIX31 <limpix31@gmail.com>
2022-10-30 09:33:06 +0200

    feat: remove newless fn

--- src/utils/class.ts
+++ src/utils/class.ts
@@ -1,6 +1,3 @@
-export const newless = <A extends any[], T>(constructor: new (...args: A) => T) =>
-  (...args: A) => new constructor(...args)
-
 export const namedClass =  <T extends object>(constructor: T, name: string): T => {
   Reflect.defineProperty(constructor, 'name', { writable: false, value: name })
   return constructor
