80fa85ec196cc5de5775a077fc30639ea705073d
LIMPIX31 <limpix31@gmail.com>
2022-10-30 13:37:09 +0200

    feat: new find utilities

--- src/lexer/find.ts
+++ src/lexer/find.ts
@@ -5,3 +5,18 @@ export const single = (symbol: string) => (src: string) =>
   src[0] === symbol ? 1 : -1
 
 export const eof = (src: string) => src.length === 0 ? 0 : -1
+
+export const ifMatch = (
+  origin: (src: string) => number,
+  then: () => void,
+  els?: () => void
+) => (src: string) => {
+  const result = origin(src)
+  result === -1 ? els?.() : then()
+  return result
+}
+
+export const matchIf = (
+  value: () => boolean,
+  origin: (src: string) => number
+) => (src: string) => value() ? origin(src) : -1
