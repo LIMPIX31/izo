1404fde4df73b70f73168ca84636c9d3a2ebf6d2
LIMPIX31 <limpix31@gmail.com>
2022-11-02 19:35:58 +0100

    feat: simplify token class

--- src/lexer/token.ts
+++ src/lexer/token.ts
@@ -1,10 +1,9 @@
-import { namedClass } from '../utils/class'
 import { Display } from '../utils/string'
 import { Span } from './span'
 import { AbstractToken } from './errors'
 
-export abstract class Token implements Display {
-  protected constructor(readonly value: string, readonly span: Span) {}
+export class Token implements Display {
+  constructor(readonly value: string, readonly span: Span) {}
 
   display(): string {
     return `(${this.span.start}:${this.span.end}) [${this.constructor.name}: ${this.value}]`
@@ -13,20 +12,4 @@ export abstract class Token implements Display {
   static tokenize(src: string): number {
     throw new AbstractToken
   }
-
-  static kind(name: string, tokenize?: (src: string) => number) {
-    const Clazz = namedClass(
-      class extends Token {
-        static tokenize = tokenize ?? Token.tokenize
-        static value = (value: string, span: Span): Token => new Clazz(value, span)
-      },
-      name)
-    return Clazz
-  }
-
-  static value(value: string, span: Span): Token {
-    throw new AbstractToken
-  }
 }
-
-export type TokenConstructor = typeof Token
