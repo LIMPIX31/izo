1d95726da406b59327211e8ba892599da122ddc7
LIMPIX31 <limpix31@gmail.com>
2022-11-02 19:29:28 +0100

    feat: rename error

--- src/lexer/errors.ts
+++ src/lexer/errors.ts
@@ -1,4 +1,5 @@
 import { error } from '../utils/error'
 
 export const AbstractToken = error('AbstractToken', 'Abstract token can\'t be used')
-export const UnexpectedEndOfFile = error('UnexpectedEndOfFile', 'Unexpected end of file')
+export const UnexpectedToken = error('UnexpectedToken', 'Unexpected token')
+export const AnonymousToken = error('AnonymousToken', 'Anonymous tokens are not allowed')
