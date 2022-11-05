9fd97749b7b7885edce1894e33bb5dd15b93203e
LIMPIX31 <limpix31@gmail.com>
2022-11-05 17:16:07 +0100

    fix: span

--- packages/common/lexer/span.ts
+++ packages/common/lexer/span.ts
@@ -10,11 +10,11 @@ export class Span {
   }
 
   get line() {
-    return (this.src.slice(this.start).match(/\n/g) || '').length
+    return (this.src.slice(0, this.start).match(/\n/g) || '').length
   }
 
   get column() {
-    const lines = this.src.slice(this.start).split('\n')
+    const lines = this.src.slice(0, this.start).split('\n')
     if(lines.length === 1) return this.start
     return this.start - lines.slice(0, lines.length - 1).join('\n').length - 1
   }
